12e5d7ab2eaf8c38366a2ddd4ff7d88f2dd03448
tyrone.wei <tyrone.wei@topnetwork.org>
2019-11-08 14:52:48 +0800

    init commit

---
+++ src/test/java/com/tyrone/baseframework/BaseframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrone.baseframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseframeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
